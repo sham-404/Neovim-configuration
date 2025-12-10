3758f7e11b6b3b9c752dab3b6a877b765e794ec6
sham-404 <shamsujith4@gmail.com>
2025-12-10 12:16:52 +0530

    minor changes for keymappings

--- lua/plugins/builtin/telescope.lua
+++ lua/plugins/builtin/telescope.lua
@@ -2,12 +2,12 @@ return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	cmd = "Telescope",
-	keys = {
-		{ "<leader>ff", "<cmd>Telescope find_files<CR>" },
-		{ "<leader>fg", "<cmd>Telescope live_grep<CR>" },
-		{ "<leader>fb", "<cmd>Telescope buffers<CR>" },
-		{ "<leader>fh", "<cmd>Telescope help_tags<CR>" },
-	},
+	-- keys = {
+	-- 	{ "<leader>ff", "<cmd>Telescope find_files<CR>", { desc = "Find Files" } },
+	-- 	{ "<leader>fg", "<cmd>Telescope live_grep<CR>", { desc = "Live Grep" } },
+	-- 	{ "<leader>fb", "<cmd>Telescope buffers<CR>", { desc = "List Buffers" } },
+	-- 	{ "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Help Tags" } },
+	-- },
 	config = function()
 		require("telescope").setup({})
 	end,
