03513594d12c222a79a866d7b7d884d4c27204af
sham-404 <shamsujith4@gmail.com>
2026-03-19 13:43:57 +0530

    Changed format_on_save to false

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -2,10 +2,7 @@ return {
   "stevearc/conform.nvim",
   event = { "BufWritePre" },
   opts = {
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
+    format_on_save = false,
     formatters_by_ft = {
       lua = { "stylua" },
       javascript = { "prettierd", "prettier" },
